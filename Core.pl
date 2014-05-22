40207d9ca695ae9e7b6c05d0b7b0b02aa09da727
thelollies <rorystephenson@gmail.com>
2014-05-23 11:43:33 +1200

    ...

--- Core.pl
+++ Core.pl
@@ -19,10 +19,45 @@ street(hamilton, auckland).
 
 actionStreet(A,B):- street(A,B); street(B,A).
 
-% Fix this first part of giveSolution. Shouldn't allow going from same start finish
-giveSolution(P, Start, End, Steps):- giveSolution(P, X, End, Steps, [Start]).
-giveSolution(_, B, B, Steps, _):- Steps=[].
-giveSolution(P, Start, End, [street(A,B)|Steps], Visited):- 
+giveSolution(P, Start, End, [Start|Steps]):- 
+	(var(End) -> X=y ; X=f), % Sets X to indicate whether End was set
+	giveSolution(P, Start, End, Steps, [Start]), 
+	not(Start == End),
+	(X == y -> setConnected([Start|Steps]); true). % If End was not set, remove results from unreachable nodes list.
+giveSolution(_, B, B, [], _).
+giveSolution(P, Start, End, [X|Steps], Visited):- 
 	call(P, Start, X), 
-	not(member(X, Visited)), 
+	not(member(X, Visited)),
 	giveSolution(P, X, End, Steps, [X|Visited]).
+
+setConnected(Nodes):- setConnected(Nodes, Nodes).
+setConnected([],_).
+setConnected([Current|ToSave], Connected):- 
+	getNodeReachables(Current, SavedReach), 
+	subtract(Reachable, Connected, Result),
+	nb_setval(Current, Result),
+	setConnected(ToSave, Connected).
+
+getNodeReachables(Node, Reachable):- 
+	catch(nb_getval(Node, Reachable), 
+        error(_,_Context),
+        Reachable = []).
+
+allCities(Result):- 
+	findall(A,actionStreet(A,_),ListWith),
+	sort(ListWith,Result).
+
+
+node(Elem, SubNodes).
+%artPoints(Nodes, ArtPoints):- 
+
+/*makeTree(_, City, [], _, Tree):- Tree = node(City, []).
+makeTree(P, City)
+makeTree(P, City, Cities, Visited, Tree):- */
+
+
+neighboursIn(P, [H|Possible] , City, Result):- 
+	(call(P, City, H) -> Res = H; Res = []),
+	neighboursIn(P, Possible, City, Rest),
+	Result = [Res | Rest].  
+neighboursIn(_, [], _, Result):- Result = [].
